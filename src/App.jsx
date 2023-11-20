5a0c50b872cfa08e35477d0963857c06f96c5a13
Username-Ivan3840 <ihripun2008@yandex.ru>
2023-11-20 17:42:27 +0300

    create project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css';
+
+function App() {
+  return (
+ <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, saepe vero voluptates consequatur, impedit blanditiis accusantium assumenda atque inventore libero laborum. Laboriosam labore culpa itaque minus totam. Veniam, tempore deleniti.</h1>
+ 
+ );
+}
+
+export default App;
