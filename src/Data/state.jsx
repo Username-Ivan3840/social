5809a76269d7a262714752fcbdfb97799530b403
Username-Ivan3840 <ihripun2008@mail.ru>
2024-03-04 17:04:46 +0300

    props

---
+++ src/Data/state.jsx
@@ -0,0 +1,8 @@
+import avatar3 from "./../img/avatar3.jpg"
+import avatar4 from "./../img/avatar4.jpg"
+import avatar2 from "./../img/avatar2.png"
+let state = {
+    dialogData: [{ name: "Elonte Mask", text: "hello!", id: "1" }, { name: "Bill gets", text: "Hi!", id: "2" }, { name: "Vasua Ivanov", text: "hi!Go Mars", id: "3" }, { name: "Ivan Ivanov", text: "Go go", id: "4" }],
+    postData: [{ name: "stiv djobs", text: "messeng for you", img: avatar2, nuberLike: "5" }, { name: "Petua Grishin", text: "some text for you!", img: avatar3, nuberLike: "23" }, { name: "Vasua Ivanov", text: "some text in frops", img: avatar4, nuberLike: "30" }]
+}
+export default state
--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import avatar3 from "./img/avatar3.jpg"
-import avatar4 from "./img/avatar4.jpg"
-import avatar2 from "./img/avatar2.png"
-let dialogData = [{ name: "Elonte Mask", text: "hello!", id: "1" }, { name: "Bill gets", text: "Hi!", id: "2" }, { name: "Vasua Ivanov", text: "hi!Go Mars", id: "3" }, { name: "Ivan Ivanov", text: "Go go", id: "4" }]
-let postData = [{ name: "stiv djobs", text: "messeng for you", img: avatar2, nuberLike: "5" }, { name: "Petua Grishin", text: "some text for you!", img: avatar3, nuberLike: "23" }, { name: "Vasua Ivanov", text: "some text in frops", img: avatar4, nuberLike: "30" }]
+import state from './Data/state';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <App dialogData={dialogData} postData={postData} />
+  <App dialogData={state.dialogData} postData={state.postData} />
 
 
 );
